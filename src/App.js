76841d503bfd2592a8f3f33e68c99c1066328ef7
Heydar Shams <shamsheidar.79@gmail.com>
2022-07-16 11:02:03 +0430

    make 'header' component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 
 import Header from './Header.js'
+import List from './List.js'
 
 function App() {
   return (
     <main className="app">
       <Header/>
+      <List>
+        {{
+          'IP Address': '192.212.174.101',
+          'Location': 'Brooklyn, NY 10001',
+          'Timezone': 'UTC -05:00',
+          'ISP': 'SpaceX Starlink',
+        }}
+      </List>
     </main>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -5,16 +5,8 @@ import List from './List.js'
 
 function Header() {
   return (
-    <header>
+    <header className='header'>
       <Input onSubmit={(str) => console.log(str)}/>
-      <List>
-        {{
-          'IP Address': '192.212.174.101',
-          'Location': 'Brooklyn, NY 10001',
-          'Timezone': 'UTC -05:00',
-          'ISP': 'SpaceX Starlink',
-        }}
-      </List>
     </header>
   );
 }
